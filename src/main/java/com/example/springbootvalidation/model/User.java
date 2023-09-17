5a6cee147da0cc5c9da332e7b5d0382bd97bb8d2
hamzeh-pourshabanan <hamzeh.p89@gmail.com>
2023-09-18 00:58:19 +0330

    feat: Add validation for grade

--- src/main/java/com/example/springbootvalidation/model/User.java
+++ src/main/java/com/example/springbootvalidation/model/User.java
@@ -1,13 +1,19 @@
 package com.example.springbootvalidation.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
 
 public class User {
 
     @NotBlank(message = "username cannot be empty")
     private String name;
     @NotBlank(message = "password cannot be empty")
+    @Length(min = 6, max = 10, message = "length of password should be between 6 to 10 characters")
     private String password;
+    @Min(value = 0)
+    @Max(value = 100 , message = "grade should not exceed 100")
     private Double grade;
 
     public User(String name, String passwork, Double grade) {
