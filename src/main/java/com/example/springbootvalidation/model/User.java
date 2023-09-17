52b0ae893ae219672644902b1a89ff7677ec69f0
hamzeh-pourshabanan <hamzeh.p89@gmail.com>
2023-09-17 23:24:42 +0330

    feat: Create User model

--- src/main/java/com/example/springbootvalidation/model/User.java
+++ src/main/java/com/example/springbootvalidation/model/User.java
@@ -2,5 +2,37 @@ package com.example.springbootvalidation.model;
 
 public class User {
 
-    
+    private String name;
+    private String passwork;
+    private Double grade;
+
+    public User(String name, String passwork, Double grade) {
+        this.name = name;
+        this.passwork = passwork;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPasswork() {
+        return passwork;
+    }
+
+    public void setPasswork(String passwork) {
+        this.passwork = passwork;
+    }
+
+    public Double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Double grade) {
+        this.grade = grade;
+    }
 }
